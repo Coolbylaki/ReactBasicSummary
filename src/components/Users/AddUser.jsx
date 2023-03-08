59e1996683ddf9b7db2a71e7e391aa5ff18791d2
Lazar Stojanović <lakisd98@gmail.com>
2023-03-08 06:11:24 +0100

    Update: error module backdrop and close button

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -8,15 +8,24 @@ import ErrorModule from "../UI/ErrorModal/ErrorModal";
 const AddUser = (props) => {
 	const [enteredUsername, setEnteredUsername] = useState("");
 	const [enteredAge, setEnteredAge] = useState("");
+	const [error, setError] = useState();
 
 	const addUserHandler = (e) => {
 		e.preventDefault();
 
 		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+			setError({
+				title: "Invalid input",
+				message: "Please enter a valid name and age (non-empty values).",
+			});
 			return;
 		}
 
 		if (+enteredAge < 1) {
+			setError({
+				title: "Invalid age",
+				message: "Please enter a valid age (bigger than zero)).",
+			});
 			return;
 		}
 
@@ -34,9 +43,11 @@ const AddUser = (props) => {
 		setEnteredAge(e.target.value);
 	};
 
+	const errorHandler = () => setError(null);
+
 	return (
 		<div>
-			<ErrorModule title="An Error occured!" message="Something went wrong!" />
+			{error && <ErrorModule title={error.title} message={error.message} onConfirm={errorHandler} />}
 			<Card className={styles.input}>
 				<form onSubmit={addUserHandler}>
 					<label htmlFor="username">Username</label>
