d3450a4650307cbc9af7d83258e456456215eab3
Lazar Stojanović <lakisd98@gmail.com>
2023-03-08 05:09:12 +0100

    Add: basic validation to add user form

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -10,6 +10,19 @@ const AddUser = (props) => {
 
 	const addUserHandler = (e) => {
 		e.preventDefault();
+
+		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+			return;
+		}
+
+		if (+enteredAge < 1) {
+			return;
+		}
+
+		console.log(enteredUsername, enteredAge);
+
+		setEnteredUsername("");
+		setEnteredAge("");
 	};
 
 	const usernameChangeHandler = (e) => {
@@ -24,9 +37,9 @@ const AddUser = (props) => {
 		<Card className={styles.input}>
 			<form onSubmit={addUserHandler}>
 				<label htmlFor="username">Username</label>
-				<input id="username" type="text" onChange={usernameChangeHandler} />
+				<input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
 				<label htmlFor="age">Age (Years)</label>
-				<input id="age" type="number" onChange={ageChangeHandler} />
+				<input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
 				<Button type="submit">Add User</Button>
 			</form>
 		</Card>
